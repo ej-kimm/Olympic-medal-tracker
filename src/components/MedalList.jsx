b1d05a49efb87b89bce1d0173cd5e3fd3f3b054f
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-10-31 13:41:33 +0900

    💄 Add sorting functionality for medals

--- src/components/MedalList.jsx
+++ src/components/MedalList.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from './ui/Button'
 import styles from '../styles/MedalList.module.css'
 
+const arrow = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    style={{ marginLeft: '5px', verticalAlign: 'middle' }}
+  >
+    <path d="M8 12.5l-5-5h10l-5 5z" />
+  </svg>
+)
+
 export default function MedalList({ medals, setMedals }) {
-  const sortedMedals = [...medals].sort((a, b) => b.gold - a.gold)
+  const [arrowIndex, setArrowIndex] = useState({
+    gold: true,
+    silver: false,
+    bronze: false,
+  })
+  const [sortedMedals, setSortedMedals] = useState([])
 
   const handleDelete = (country) => {
     setMedals(medals.filter((medal) => medal.country !== country))
   }
+  const handleArrowClick = (type) => {
+    setArrowIndex({
+      gold: type === 'gold',
+      silver: type === 'silver',
+      bronze: type === 'bronze',
+    })
+  }
+
+  useEffect(() => {
+    const type = arrowIndex.gold
+      ? 'gold'
+      : arrowIndex.silver
+      ? 'silver'
+      : 'bronze'
+    const newSortedMedals = [...medals].sort((a, b) => {
+      if (type === 'gold') return b.gold - a.gold
+      if (type === 'silver') return b.silver - a.silver
+      if (type === 'bronze') return b.bronze - a.bronze
+    })
+    setSortedMedals(newSortedMedals)
+  }, [medals, arrowIndex])
 
   return (
     <>
@@ -19,9 +58,30 @@ export default function MedalList({ medals, setMedals }) {
           <thead>
             <tr>
               <th>국가명</th>
-              <th>금메달</th>
-              <th>은메달</th>
-              <th>동메달</th>
+              <th>
+                <button type="button" onClick={() => handleArrowClick('gold')}>
+                  금메달
+                  {arrowIndex.gold && arrow}
+                </button>
+              </th>
+              <th>
+                <button
+                  type="button"
+                  onClick={() => handleArrowClick('silver')}
+                >
+                  은메달
+                  {arrowIndex.silver && arrow}
+                </button>
+              </th>
+              <th>
+                <button
+                  type="button"
+                  onClick={() => handleArrowClick('bronze')}
+                >
+                  동메달
+                  {arrowIndex.bronze && arrow}
+                </button>
+              </th>
               <th>액션</th>
             </tr>
           </thead>
